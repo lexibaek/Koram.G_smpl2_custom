31953e46fe496be8b63d5ab741493846706fcabd
Erik Onarheim <erik.onarheim@gmail.com>
2024-12-25 19:58:31 -0600

    update ldtk sample

--- vite.config.js
+++ vite.config.js
@@ -19,28 +19,8 @@ const tiledPlugin = () => {
 
 export default defineConfig({
     // plugins: [tiledPlugin()], // hint vite that tiled tilesets should be treated as external
-    // currently excalibur plugins are commonjs
-    // this forces vite to keep things from bundling ESM together with commonjs
-    optimizeDeps: {
-        exclude: ["excalibur"],
-    },
     build: {
         assetsInlineLimit: 0, // excalibur cannot handle inlined xml in prod mode
-        sourcemap: true,
-        // Vite uses rollup currently for prod builds so a separate config is needed
-        // to keep vite from bundling ESM together with commonjs
-        rollupOptions: {
-            output: {
-                // interop: "compat",
-                format: 'umd',
-                // inlineDynamicImports: false,
-                // manualChunks: (id, {getModuleInfo, getModuleIds}) => {
-                //     console.log(id);
-                //     if (id.includes('/esm/excalibur.js')) {
-                //         return 'e'
-                //     }
-                // }
-            },
-        }
+        sourcemap: true
     }
 });
